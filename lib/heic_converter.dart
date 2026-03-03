f8488db3a151a0f2526b9c7af1f16b28e113022f
Thomas Beck <tb@allesimgriff.de>
2026-03-03 16:07:55 +0100

    HEIC Konverter auf package:js umgestellt

--- lib/heic_converter.dart
+++ lib/heic_converter.dart
@@ -1,54 +1,21 @@
-import 'dart:js_interop';
+@JS()
+library heic_converter;
+
+import 'package:js/js.dart';
+import 'package:js/js_util.dart' show promiseToFuture;
 import 'dart:typed_data';
 
 @JS('heic2any')
-external JSPromise _heic2any(Heic2anyOptions options);
+external dynamic _heic2any(dynamic options);
 
 @JS()
 @anonymous
 class Heic2anyOptions {
-  external Uint8List get blob;
-  external String get toType;
-  external bool get multiple;
-
-  factory Heic2anyOptions({
-    required Uint8List blob,
-    required String toType,
-    required bool multiple,
-  }) => Heic2anyOptionsImpl(blob: blob, toType: toType, multiple: multiple);
-}
-
-@JS()
-@anonymous
-class Heic2anyOptionsImpl implements Heic2anyOptions {
-  external factory Heic2anyOptionsImpl({
-    required Uint8List blob,
-    required String toType,
-    required bool multiple,
+  external factory Heic2anyOptions({
+    dynamic blob,
+    String? toType,
+    bool? multiple,
   });
-
-  external Uint8List get blob;
-  external String get toType;
-  external bool get multiple;
-}
-
-@JS()
-@anonymous
-class JSBlob {
-  external JSPromise arrayBuffer();
-}
-
-@JS()
-@anonymous
-class JSArray {
-  external dynamic operator [](int index);
-}
-
-@JS()
-@anonymous
-class JSPromise {
-  external JSPromise then(JSFunction onFulfilled);
-  external Future<dynamic> toDart;
 }
 
 Future<Uint8List> convertHeicToJpg(Uint8List heicData) async {
@@ -59,18 +26,14 @@ Future<Uint8List> convertHeicToJpg(Uint8List heicData) async {
       multiple: false,
     );
 
-    final result = await _heic2any(options).toDart;
+    final result = await promiseToFuture(_heic2any(options));
 
-    if (result is JSArray) {
-      final blob = result[0] as JSBlob;
-      final arrayBuffer = await blob.arrayBuffer().toDart;
-      return arrayBuffer.toUint8List();
-    } else if (result is JSBlob) {
-      final arrayBuffer = await result.arrayBuffer().toDart;
-      return arrayBuffer.toUint8List();
-    }
+    // heic2any returns a Blob, convert to Uint8List
+    final arrayBuffer = await promiseToFuture(
+      callMethod(result, 'arrayBuffer', []),
+    );
 
-    throw Exception('Unerwartetes HEIC Konvertierungsergebnis');
+    return allowInterop(() => arrayBuffer) as Uint8List;
   } catch (e) {
     throw Exception('HEIC Konvertierung fehlgeschlagen: $e');
   }
