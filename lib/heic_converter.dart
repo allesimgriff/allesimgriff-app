a1768e1bfdd6c15d87298b17273bb3d28bba8fc8
Thomas Beck <tb@allesimgriff.de>
2026-03-03 16:33:53 +0100

    dart:js_util statt package:js für Flutter Web

--- lib/heic_converter.dart
+++ lib/heic_converter.dart
@@ -1,39 +1,42 @@
 @JS()
 library heic_converter;
 
-import 'package:js/js.dart';
-import 'package:js/js_util.dart' show promiseToFuture;
+import 'dart:js_interop';
+import 'dart:js_util' as js_util;
 import 'dart:typed_data';
 
 @JS('heic2any')
-external dynamic _heic2any(dynamic options);
+external JSPromise _heic2any(Heic2anyOptions options);
 
 @JS()
 @anonymous
 class Heic2anyOptions {
   external factory Heic2anyOptions({
-    dynamic blob,
+    Object? blob,
     String? toType,
     bool? multiple,
   });
 }
 
+@JS()
+@anonymous
+class JSBlob {
+  external JSPromise arrayBuffer();
+}
+
 Future<Uint8List> convertHeicToJpg(Uint8List heicData) async {
   try {
     final options = Heic2anyOptions(
-      blob: heicData,
+      blob: heicData.toJS,
       toType: 'image/jpeg',
       multiple: false,
     );
 
-    final result = await promiseToFuture(_heic2any(options));
+    final jsResult = await _heic2any(options).toDart;
+    final blob = jsResult as JSBlob;
+    final arrayBuffer = await blob.arrayBuffer().toDart;
 
-    // heic2any returns a Blob, convert to Uint8List
-    final arrayBuffer = await promiseToFuture(
-      callMethod(result, 'arrayBuffer', []),
-    );
-
-    return allowInterop(() => arrayBuffer) as Uint8List;
+    return arrayBuffer.toDart;
   } catch (e) {
     throw Exception('HEIC Konvertierung fehlgeschlagen: $e');
   }
